7db9fecb77c9bfaef0c27b9866a2f20fcb3c51b9
bonie3d <inmajimpe9@gmail.com>
2021-09-21 10:47:14 +0200

    Ejercicio 1.c

--- PR01/Assets/Scripts/Ejercicio1.cs
+++ PR01/Assets/Scripts/Ejercicio1.cs
@@ -6,15 +6,20 @@ public class Ejercicio1 : MonoBehaviour
 {
     [SerializeField] int num1 = 10;
     [SerializeField] float dec1 = 5.9f;
-    [SerializeField] bool bool1 = false;
-    [SerializeField] string text1 = "Hola mundo";
+    bool bool1 = false;
+    string text1 = "Hola mundo";
+    float multiplicar;
     // Start is called before the first frame update
     void Start()
     {
-        print(num1 = 9);
-        print(text1 = "Adios");
-        print(bool1 = true);
-        print(dec1 = 2.5f);
+        float resultado;
+        resultado = num1 + dec1;
+        print(resultado);
+        multiplicar = num1 * dec1;
+        print(multiplicar);
+        print("resultado" + multiplicar);
+        print("Esta vivo" + bool1);
+       
     }
 
     // Update is called once per frame
