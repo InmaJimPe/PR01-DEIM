9c9e151368de7c584f8d3a7898ce5d79098cef91
bonie3d <inmajimpe9@gmail.com>
2021-09-21 09:56:46 +0200

    Ejercicio1.a

---
+++ PR01/Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    private int num1 = 10;
+    private float dec1 = 5.9f;
+    private bool bool1 = false;
+    private string text1 = "Hola mundo";
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(num1);
+        print(text1);
+        print(bool1);
+        print(dec1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
