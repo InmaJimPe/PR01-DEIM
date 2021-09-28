3f282eaf2c2eedeceba3a25f662e1c04d2eb8047
bonie3d <inmajimpe9@gmail.com>
2021-09-28 11:50:36 +0200

    3.b

--- PR01/Assets/Scripts/Ejercicio1.cs
+++ PR01/Assets/Scripts/Ejercicio1.cs
@@ -4,36 +4,75 @@ using UnityEngine;
 
 public class Ejercicio1 : MonoBehaviour
 {
-    [SerializeField] string nombre;
+    [SerializeField] float num1;
+    [SerializeField] float num2;
+    [SerializeField] string op;
+
+    bool par = true;
     // Start is called before the first frame update
     void Start()
     {
-       
-        
+        Calcular(num1, num2, op);
+
 
     }
-    void Saludos(string nombre)
-    {
 
-        print("saludos " + nombre);
-
-    }
-    
-    
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown("space"))
         {
-
-            Saludos(nombre);
+            Calcular(num1, num2, op);
 
         }
+    }
+
+    void Calcular(float num1, float num2, string op)
+    {
+        float resultado = 0f;
+        if (op == "dividir")
+        {
+
+            resultado = num1 / num2;
+
+        }
+        else if (op == "multiplicar")
+        {
+
+            resultado = num1 * num2;
+
+        }
+        else if (op == "sumar")
+        {
+
+            resultado = num1 + num2;
+
+        }
+        else if (op == "restar")
+        {
+
+            resultado = num1 - num2;
+
+        }
+        print(resultado);
+        resultado = resultado%2;
+
+        if(resultado == 0)
+        {
+            print("Par ");
+            par = true;
+        }
+        else
+        {
+            print("Impar ");
+            par = false;
+        }
+
 
 
     }
 
-    
+
 
 
 
